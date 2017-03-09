77219eaed2319f8ab6643340b8e05a8432baf1be
BeLikeYangY <yy93618400>
2017-03-08 20:36:33 -0500

    download firebase data

--- app/src/main/java/com/example/android/secretholder/MainActivity.java
+++ app/src/main/java/com/example/android/secretholder/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.android.secretholder;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -28,5 +29,8 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void reveal(View view) {
+        Intent intent = new Intent(this, Main2Activity.class);
+        startActivity(intent);
+
     }
 }
